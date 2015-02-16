8d3aacad690fcf8a1bfbe1af0df4d527673f6a6b
Justin Sternberg <justin@dsgnwrks.pro>
2015-02-16 17:09:47 -0500

    Properly enqueue resources. We don't want to do it on EVERY admin page.. only if field is loaded. Also add version for busting cache w/ new versions of the plugin.

--- cmb2_field_slider.php
+++ cmb2_field_slider.php
@@ -7,17 +7,21 @@
  * Author:      Mateusz Krupnik
  * License:     GPLv2+
  */
- 
+
 
 class OWN_Field_Slider {
 
+	const VERSION = '0.1.0';
+
 	public function hooks() {
 		add_filter( 'cmb2_render_own_slider',  array( $this, 'own_slider_field' ), 10, 5 );
-		add_action( 'admin_enqueue_scripts', array( $this, 'setup_admin_scripts' ) );
 	}
 
 	public function own_slider_field( $field_args, $field_escaped_value, $field_object_id, $field_object_type, $field_type_object ) {
-	
+
+		// Only enqueue scripts if field is used.
+		$this->setup_admin_scripts();
+
 		$value = $field_args->escaped_value() ? $field_args->escaped_value() : $field_args->start_value();
 
 		echo '<input type="hidden" id="start" value="'.$value.'">';
@@ -25,21 +29,20 @@ class OWN_Field_Slider {
 		echo '<input type="hidden" id="max" value="'.$field_args->max().'">';
 
 		echo '<div id="slider"></div>';
-			
+
 		echo '<input type="hidden" id="amount" name="' . $field_args->id() . '" readonly />';
-			
-		echo '<p class="cmb2-metabox-description">'.$field_args->desc().'<span id="value"></span></p>';	 
+
+		echo '<p class="cmb2-metabox-description">'.$field_args->desc().'<span id="value"></span></p>';
 	}
 
-	 public function setup_admin_scripts( ) {
-	 
-	 	wp_enqueue_script( 'jquery-ui-slider');
-		wp_enqueue_script( 'cmb2_field_slider_js',  plugins_url( 'js/cmb2_field_slider.js', __FILE__ ), array( 'jquery' ), null );
-		
-		wp_enqueue_style( 'slider_ui', 'http://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css', array(), '1.0', 'all' );
-		wp_enqueue_style( 'cmb2_field_slider_css', plugins_url( 'css/cmb2_field_slider.css', __FILE__ ), array(), '1.0', 'all' );
+	public function setup_admin_scripts( ) {
 
-	} 
+		wp_enqueue_script( 'cmb2_field_slider_js',  plugins_url( 'js/cmb2_field_slider.js', __FILE__ ), array( 'jquery', 'jquery-ui-slider' ), self::VERSION );
+
+		wp_register_style( 'slider_ui', 'http://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css', array(), '1.0' );
+		wp_enqueue_style( 'cmb2_field_slider_css', plugins_url( 'css/cmb2_field_slider.css', __FILE__ ), array( 'slider_ui' ), self::VERSION );
+
+	}
 }
 $own_field_slider = new OWN_Field_Slider();
 $own_field_slider->hooks();
